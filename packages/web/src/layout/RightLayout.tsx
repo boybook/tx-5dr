12497a464a238842b7d374b972d96c3a0b99ceeb
Luoyumoe <i@luotianyi.eu>
2026-07-04 03:21:37 +0800

    fix(layout): split mobile digital right workspace evenly

--- packages/web/src/layout/RightLayout.tsx
+++ packages/web/src/layout/RightLayout.tsx
@@ -373,10 +373,10 @@ export const RightLayout: React.FC = () => {
       <div className="flex-1 p-2 pt-0 md:p-5 md:pt-0 flex flex-col gap-2 md:gap-4 min-h-0 overflow-hidden">
         {isMobile ? (
           <>
-            <div className="relative z-0 flex-1 min-h-0 overflow-hidden">
+            <div className="relative z-0 min-h-0 flex-1 overflow-hidden">
               <MyRelatedFramesTable className="h-full" />
             </div>
-            <div className="relative z-10 flex-shrink-0">
+            <div className="relative z-10 min-h-0 flex-1 overflow-y-auto overscroll-contain">
               <RadioOperatorList onCreateOperator={handleCreateOperator} />
             </div>
           </>
